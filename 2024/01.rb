f068e50c081be8bfebf140e0163dd454460ed881
Paulo Ancheta <paulo.ancheta@gmail.com>
2024-12-02 12:33:10 -0800

    clean

--- 2024/01.rb
+++ 2024/01.rb
@@ -1,22 +1,20 @@
-require 'pry'
-
-actual_input = File.read("01_input.txt")
+actual_input = File.read('01_input.txt')
 test_input = <<~INPUT
-3   4
-4   3
-2   5
-1   3
-3   9
-3   3
+  3   4
+  4   3
+  2   5
+  1   3
+  3   9
+  3   3
 INPUT
 
-is_actual = ENV["ACTUAL"]
+is_actual = ENV['ACTUAL']
 input = is_actual ? actual_input : test_input
 
 # transform input to strings
 parsed_input = input.split("\n").map do |string|
-  matches = string.match(/(\d+)\s+(\d+)/)
-  [matches[1].to_i, matches[2].to_i]
+  _, first, second = string.match(/(\d+)\s+(\d+)/).to_a
+  [first.to_i, second.to_i]
 end
 
 # sort
