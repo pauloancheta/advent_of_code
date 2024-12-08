e9ee1b5f1884e247c3d0e27d8be127c256795f2d
Paulo Ancheta <paulo.ancheta@gmail.com>
2024-12-07 20:01:45 -0800

    I think it's working?

--- 2024/06.rb
+++ 2024/06.rb
@@ -7,7 +7,7 @@ test_input = <<~INPUT
   ..........
   ..#.......
   .......#..
-  ..........
+  ....0.....
   .#..^.....
   ........#.
   #.........
@@ -21,6 +21,10 @@ input = input.split("\n").map { |input| input.split('') }
 
 @can_move = true
 @loop = 0
+@maybe_loop = 0
+@looped = true
+@block_blacklist = []
+@runlist = []
 
 def vis(input)
   system "clear"
@@ -28,7 +32,10 @@ def vis(input)
     row.each { |char| print char }
     puts
   end
-  sleep 0.1
+
+  print "maybe_loop #{@maybe_loop}\n"
+  print "loop #{@loop}\n"
+  print "block_blacklist #{@block_blacklist}\n"
 end
 
 def guard?(char)
@@ -50,15 +57,8 @@ def next_move(x, y, guard)
   return [x + 1, y] if guard == '>'
 end
 
-def previous_move(x, y, guard)
-  return [x, y + 1] if guard == '^'
-  return [x, y - 1] if guard == 'V'
-  return [x + 1, y] if guard == '<'
-  return [x - 1, y] if guard == '>'
-end
-
 def should_move?(char)
-  return false if char == "#"
+  return false if char == "#" || char == "O"
   true
 end
 
@@ -69,41 +69,88 @@ def replace_guard(guard)
   return '^' if guard == '<'
 end
 
+def terminate(input, x, y)
+  if input[y].nil? || input[y][x].nil? || x.negative? || y.negative?
+    @can_move = false
+    return true
+  end
+
+  if @maybe_loop == 2
+    @can_move = false
+    @loop += 1
+    return true
+  end
+
+  false
+end
+
+def add_block(input, x, y, guard)
+  block_x, block_y = next_move(x, y, guard)
+  return if input[block_y].nil? || input[block_y][block_x] == "#"
+  return if has_loop?(input)
+
+  if !@block_blacklist.include?([block_x, block_y])
+    input[block_y][block_x] = "O"
+    @block_blacklist << [block_x, block_y]
+  else
+    # add_block(input, block_x, block_y, guard)
+  end
+end
+
 def move(input)
   x, y, guard = find_guard_coordinates(input)
   next_x, next_y = next_move(x, y, guard)
-  prev_x, prev_y = previous_move(x, y, guard)
 
-  if input[next_y].nil? || input[next_y][next_x].nil?
-    @can_move = false
-    input[y][x] = "X"
-    return input
-  end
+  return input if terminate(input, next_x, next_y)
 
   next_char = input[next_y][next_x]
-  prev_char = input[prev_y][prev_x]
 
   if should_move?(next_char)
+    add_block(input, next_x, next_y, guard)
     input[y][x] = "X"
-
     input[next_y][next_x] = guard
-  elsif next_char == "#"
+
+  elsif next_char == "#" || next_char == "O"
     input[y][x] = replace_guard(guard)
+
+    @maybe_loop += 1 if next_char == "O"
   end
 
   input
 end
 
+def has_loop?(input)
+  input.flatten.join.include?("O")
+end
+
+def restart!(input, x, y, guard)
+  new_x, new_y, _guard = find_guard_coordinates(input)
+  input[new_y][new_x] = "."
+
+  input.each_with_index do |row, y|
+    row.each_with_index do |char, x|
+      input[y][x] = "." if char == "O" || char == "X"
+    end
+  end
+
+  input[y][x] = guard
+  @can_move = true
+  @maybe_loop = 0
+end
+
 def run(input)
+  original_x, original_y, original_guard = find_guard_coordinates(input)
   while @can_move
     if ENV["ACTUAL"]
       move(input)
     else
       vis move(input)
     end
+
+    restart!(input, original_x, original_y, original_guard) if has_loop?(input) && !@can_move
   end
 end
 
 run(input)
-puts "Part 1: #{input.flatten.join.count("X")}"
+puts "Part 1: #{input.flatten.join.count("X") + 1}"
 puts "Part 2: #{@loop}"
