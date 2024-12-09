5421127db054da1524dec842f598cb963aebb97b
Paulo Ancheta <paulo.ancheta@gmail.com>
2024-12-08 16:29:58 -0800

    replace the old bad code

--- 2024/06.rb
+++ 2024/06.rb
@@ -1,156 +1,145 @@
 # rubocop:disable all
 
 require 'pry'
+
 test_input = <<~INPUT
   ....#.....
   .........#
   ..........
   ..#.......
   .......#..
-  ....0.....
+  ..........
   .#..^.....
   ........#.
   #.........
   ......#...
 INPUT
-test_input = test_input
 actual_input = File.read('06_input.txt')
 
 input = ENV['ACTUAL'].nil? ? test_input : actual_input
 input = input.split("\n").map { |input| input.split('') }
 
-@can_move = true
-@loop = 0
-@maybe_loop = 0
-@looped = true
-@block_blacklist = []
-@runlist = []
+@x, @y, @move_count, @loop, @run_count = 0, 0, 0, 0, 0
+@pointer = ""
+@visited = Hash.new { |k,v| v = 0 }
+@block_history = Hash.new { |k,v| v = 0 }
+@loop_history = Hash.new { |k,v| v = 0 }
+@try_loop = true
+@current_block = []
 
 def vis(input)
-  system "clear"
   input.each do |row|
     row.each { |char| print char }
     puts
   end
-
-  print "maybe_loop #{@maybe_loop}\n"
-  print "loop #{@loop}\n"
-  print "block_blacklist #{@block_blacklist}\n"
+  puts "#{@block_history.count} loop: #{@loop}" if @run_count % 100_000 == 0
 end
 
-def guard?(char)
-  %w|^ > < V|.include?(char)
-end
-
-def find_guard_coordinates(input)
+def find_pointer(input)
+  @input = input
   input.each_with_index do |row, y|
     row.each_with_index do |char, x|
-      return [x, y, char] if guard?(char)
+      if %w[> < V ^].include?(char)
+        @x = x
+        @y = y
+        @pointer = char
+      end
     end
   end
 end
 
-def next_move(x, y, guard)
-  return [x, y - 1] if guard == '^'
-  return [x, y + 1] if guard == 'V'
-  return [x - 1, y] if guard == '<'
-  return [x + 1, y] if guard == '>'
+def move_list
+  return [1, 0] if @pointer == ">"
+  return [-1, 0] if @pointer == "<"
+  return [0, 1] if @pointer == "V"
+  return [0, -1] if @pointer == "^"
 end
 
-def should_move?(char)
-  return false if char == "#" || char == "O"
-  true
-end
-
-def replace_guard(guard)
-  return '>' if guard == '^'
-  return 'V' if guard == '>'
-  return '<' if guard == 'V'
-  return '^' if guard == '<'
-end
-
-def terminate(input, x, y)
-  if input[y].nil? || input[y][x].nil? || x.negative? || y.negative?
-    @can_move = false
-    return true
-  end
-
-  if @maybe_loop == 2
-    @can_move = false
-    @loop += 1
-    return true
-  end
-
-  false
-end
-
-def add_block(input, x, y, guard)
-  block_x, block_y = next_move(x, y, guard)
-  return if input[block_y].nil? || input[block_y][block_x] == "#"
-  return if has_loop?(input)
-
-  if !@block_blacklist.include?([block_x, block_y])
-    input[block_y][block_x] = "O"
-    @block_blacklist << [block_x, block_y]
-  else
-    # add_block(input, block_x, block_y, guard)
+def rotate_pointer
+  case @pointer
+  when ">"
+    @pointer = 'V'
+  when "V"
+    @pointer = '<'
+  when "<"
+    @pointer = '^'
+  when "^"
+    @pointer = '>'
   end
 end
 
-def move(input)
-  x, y, guard = find_guard_coordinates(input)
-  next_x, next_y = next_move(x, y, guard)
+def calculate_next_move
+  change_x, change_y = move_list
+  @next_x, @next_y = [@x + change_x, @y + change_y]
+end
 
-  return input if terminate(input, next_x, next_y)
+def step
+  @visited[[@x, @y]] += 1
+  return rotate_pointer if @input[@next_y][@next_x] == '#' || [@next_x, @next_y] == @current_block
 
-  next_char = input[next_y][next_x]
+  @x = @next_x
+  @y = @next_y
+end
 
-  if should_move?(next_char)
-    add_block(input, next_x, next_y, guard)
-    input[y][x] = "X"
-    input[next_y][next_x] = guard
+def add_blocks
+  return if !@current_block.empty?
+  return if @input[@next_y][@next_x] == "#"
 
-  elsif next_char == "#" || next_char == "O"
-    input[y][x] = replace_guard(guard)
-
-    @maybe_loop += 1 if next_char == "O"
+  if @block_history[[@next_x, @next_y]] == 0
+    @current_block = [@next_x, @next_y]
+    @block_history[@current_block] += 1
   end
-
-  input
 end
 
-def has_loop?(input)
-  input.flatten.join.include?("O")
+def out_of_bounds?
+  @input[@next_y].nil? || @input[@next_y][@next_x].nil? || @next_x.negative? || @next_y.negative?
 end
 
-def restart!(input, x, y, guard)
-  new_x, new_y, _guard = find_guard_coordinates(input)
-  input[new_y][new_x] = "."
+def reset!
+  return if @current_block.empty?
 
-  input.each_with_index do |row, y|
-    row.each_with_index do |char, x|
-      input[y][x] = "." if char == "O" || char == "X"
-    end
-  end
-
-  input[y][x] = guard
-  @can_move = true
-  @maybe_loop = 0
+  @current_block = []
+  @x, @y, @pointer = @orig_x, @orig_y, @orig_pointer
+  @visited = Hash.new { |k,v| v = 0 }
 end
 
-def run(input)
-  original_x, original_y, original_guard = find_guard_coordinates(input)
-  while @can_move
-    if ENV["ACTUAL"]
-      move(input)
+def looped?
+  @visited[[@x, @y]] > 3 # break from infinite loop
+end
+
+def run
+  loop do
+    @run_count += 1
+    calculate_next_move
+    if out_of_bounds?
+      break if @current_block.empty?
+      reset!
+    elsif looped?
+      @loop_history[@current_block] += 1
+      @loop += 1
+      reset!
     else
-      vis move(input)
+      add_blocks
+      step
     end
-
-    restart!(input, original_x, original_y, original_guard) if has_loop?(input) && !@can_move
+    @input
   end
+
+  # mark the last visited
+  @visited[[@x, @y]] += 1
 end
 
-run(input)
-puts "Part 1: #{input.flatten.join.count("X") + 1}"
-puts "Part 2: #{@loop}"
+# initialize pointer values
+find_pointer(input)
+@orig_x, @orig_y, @orig_pointer = @x, @y, @pointer
+@max_x, @max_y = @input.first.length, @input.length
+
+run
+puts @visited.count
+puts "final loop count: #{@loop_history.count}"
+puts "final block_history count: #{@block_history.count}"
+
+@loop_history.each do |key, value|
+  x, y = key
+  @input[y][x] = "O"
+end
